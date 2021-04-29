561aea863b2bfe2fdd0a246d8fc8d3d8062f14db
yamap55 <tetuya120@hotmail.com>
2021-04-29 23:26:46 +0900

    [update] bolt setting

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 """main"""
+import os
 from logging import config
 
-from hoge.huga import Huga
+from slack_bolt import App
 
 config.fileConfig("logging.conf", disable_existing_loggers=False)
 
+
+# ボットトークンと署名シークレットを使ってアプリを初期化します
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"), signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+# アプリを起動します
 if __name__ == "__main__":
-    Huga().piyo()
+    app.start(port=int(os.environ.get("PORT", 3000)))
